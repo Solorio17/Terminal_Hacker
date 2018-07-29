cb80f04b8fe8bb4205cd95813f5fd1d327382adf
Jason <jasonsoloriom@gmail.com>
2018-07-29 16:33:59 -0600

    Creating a c# script to print text out to the terminal

---
+++ Assets/hacker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Terminal.WriteLine("You've gotten this far, you know what to do..");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
