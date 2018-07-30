b98a8a55cce926ca5c05fab5cf63cdfc59181bca
Jason <jasonsoloriom@gmail.com>
2018-07-30 17:57:43 -0600

    Changing the welcome message, applying a sort of MI theme

--- Assets/hacker.cs
+++ Assets/hacker.cs
@@ -6,7 +6,26 @@ public class hacker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Terminal.WriteLine("You've gotten this far, you know what to do..");
+		ShowMainMenu();
+	}
+
+	void ShowMainMenu()
+	{
+		Terminal.WriteLine("Your mission should you choose to accept it");
+		Terminal.WriteLine("is to hack one of these locations and retrieve the secret data.");
+        Terminal.WriteLine(" ");
+        Terminal.WriteLine("01_l0cAl H1gH ScH0oL");
+		Terminal.WriteLine("02_c0lOrAd0 P0wEr gRid");
+		Terminal.WriteLine("03__nAti0NaL aEr0NauTICs & SpAC3 aDmInIsTr4Ti0n");
+	}
+
+	void OnUserInput(string input)
+	{
+		Terminal.ClearScreen();
+		if (input == "menu".ToLower());
+		{
+			ShowMainMenu();
+		}
 	}
 	
 	// Update is called once per frame
