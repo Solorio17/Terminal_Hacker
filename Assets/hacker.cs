c748b4def1565523daed7e2aa284dd221a94f973
Jason <jasonsoloriom@gmail.com>
2018-07-30 18:52:36 -0600

    Changing the player greet a bit more

--- Assets/hacker.cs
+++ Assets/hacker.cs
@@ -4,32 +4,45 @@ using UnityEngine;
 
 public class hacker : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		ShowMainMenu();
-	}
+    // Use this for initialization
+    void Start () {
+        ShowMainMenu();
+    }
 
-	void ShowMainMenu()
-	{
-		Terminal.WriteLine("Your mission should you choose to accept it");
-		Terminal.WriteLine("is to hack one of these locations and retrieve the secret data.");
+    void ShowMainMenu()
+    {
+        Terminal.WriteLine("Welcome Agent");
+        Terminal.WriteLine("Your mission should you choose to accept it is to hack one of these locations and retrieve the secret data.");
         Terminal.WriteLine(" ");
         Terminal.WriteLine("01_l0cAl H1gH ScH0oL");
-		Terminal.WriteLine("02_c0lOrAd0 P0wEr gRid");
-		Terminal.WriteLine("03__nAti0NaL aEr0NauTICs & SpAC3 aDmInIsTr4Ti0n");
-	}
+        Terminal.WriteLine("02_c0lOrAd0 P0wEr gRid");
+        Terminal.WriteLine("03__nAti0NaL aEr0NauTICs & SpAC3 aDmInIsTr4Ti0n");
+        Terminal.WriteLine(" ");
+        Terminal.WriteLine("What do you choose?");
 
-	void OnUserInput(string input)
-	{
-		Terminal.ClearScreen();
-		if (input == "menu".ToLower());
-		{
-			ShowMainMenu();
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+
+
+    void OnUserInput(string input)
+    {
+        Terminal.ClearScreen();
+        if (input == "menu".ToLower())
+        {
+            ShowMainMenu();
+        }
+        else if(input == "01")
+        {
+            Terminal.WriteLine("You have chosen 01_l0cAl H1gH ScH0oL");
+        }
+        else if(input == "02")
+        {
+            Terminal.WriteLine("You have chosen 02_c0lOrAd0 P0wEr gRid");
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
 }
